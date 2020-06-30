1ab0c5a4cc8c321bd55f18743667f5f655f7ec36
qucumbah <sc.buher@yandex.ru>
2020-06-30 23:08:16 +0300

    fix text contrast; improve settings loading

--- src/js/components/SettingsProvider.jsx
+++ src/js/components/SettingsProvider.jsx
@@ -14,12 +14,14 @@ export const SettingsProvider = ({ children }) => {
     const loadSettings = () => JSON.parse(localStorage.getItem('settings'));
     const settings = loadSettings();
 
-    if (settings !== null) {
-      setTemporaryColorHue(settings.permanentColorHue);
-      setPermanentColorHue(settings.permanentColorHue);
-      setDarkTheme(settings.darkTheme);
-      setFontSize(settings.fontSize);
+    if (settings === null) {
+      return;
     }
+
+    setTemporaryColorHue(settings?.permanentColorHue ?? temporaryColorHue);
+    setPermanentColorHue(settings?.permanentColorHue ?? permanentColorHue);
+    setDarkTheme(settings?.darkTheme ?? darkTheme);
+    setFontSize(settings?.fontSize ?? fontSize);
   }, []);
 
   useEffect(() => {
@@ -40,10 +42,6 @@ export const SettingsProvider = ({ children }) => {
     const backgroundColor = 'white';
     const overlayBackgroundColor = primaryColor;
     const bookBackgroundColor = backgroundColor;
-    const needContrastText = (
-      (temporaryColorHue > 0.1) && (temporaryColorHue < 0.2)
-    );
-    const textOverPrimaryColor = needContrastText ? contrastColor : 'white';
     const textOverBackgroundColor = 'black';
 
     return {
@@ -53,7 +51,6 @@ export const SettingsProvider = ({ children }) => {
       backgroundColor,
       overlayBackgroundColor,
       bookBackgroundColor,
-      textOverPrimaryColor,
       textOverBackgroundColor,
     };
   };
@@ -64,10 +61,6 @@ export const SettingsProvider = ({ children }) => {
     const backgroundColor = '#333';
     const overlayBackgroundColor = secondaryColor;
     const bookBackgroundColor = '#222';
-    const needContrastText = (
-      (temporaryColorHue > 0.1) && (temporaryColorHue < 0.2)
-    );
-    const textOverPrimaryColor = needContrastText ? contrastColor : 'white';
     const textOverBackgroundColor = '#aaa';
 
     return {
@@ -77,7 +70,6 @@ export const SettingsProvider = ({ children }) => {
       backgroundColor,
       overlayBackgroundColor,
       bookBackgroundColor,
-      textOverPrimaryColor,
       textOverBackgroundColor,
     };
   };
@@ -90,9 +82,46 @@ export const SettingsProvider = ({ children }) => {
       backgroundColor,
       overlayBackgroundColor,
       bookBackgroundColor,
-      textOverPrimaryColor,
       textOverBackgroundColor,
     } = (darkTheme) ? getDarkThemeColors() : getLightThemeColors();
+
+    const hslToRgb = (H, S, L) => {
+      const C = (1 - Math.abs(2 * L - 1)) * S;
+      const X = C * (1 - Math.abs(((H / 60) % 2) - 1));
+      const getRgb = () => {
+        if (H < 60) {
+          return [C, X, 0];
+        }
+        if (H < 120) {
+          return [X, C, 0];
+        }
+        if (H < 180) {
+          return [0, C, X];
+        }
+        if (H < 240) {
+          return [0, X, C];
+        }
+        if (H < 300) {
+          return [X, 0, C];
+        }
+
+        return [C, 0, X];
+      };
+
+      const m = L - C / 2;
+      return getRgb().map((colorValue) => 255 * (colorValue + m));
+    };
+
+    const needsBlackText = (hue) => {
+      const [red, green, blue] = hslToRgb(hue, 1, 0.5);
+      const brightness = (red * 299 + green * 587 + blue * 114) / 255000;
+
+      return brightness >= 0.5;
+    };
+
+    const textOverPrimaryColor = (
+      needsBlackText(temporaryColorHue) ? 'black' : 'white'
+    );
 
     const rootStyle = document.querySelector(':root').style;
     rootStyle.setProperty('--primary', primaryColor);
